97bdbc2d022cf3db6f2ddb5bd5797de18cd2a46c
Fatih Oguz <oguzfatih39@gmail.com>
2024-06-14 15:46:24 +0300

    Initial Commit

---
+++ FirstSwiftUIApp/FirstSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIAppApp.swift
+//  FirstSwiftUIApp
+//
+//  Created by Fatih Oğuz on 14.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
